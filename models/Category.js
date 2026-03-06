a153b446cb5cf9a40c341c76734412ee2fffb3f5
Lashkov Yevhen <evgene@icloud.com>
2026-03-06 13:10:56 +0100

    Added models Category and Product

---
+++ models/Category.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Category = sequelize.define(
+  "Category",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "Categories",
+    timestamps: false,
+  },
+);
+
+export default Category;
---
+++ models/Product.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Product = sequelize.define(
+  "Product",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "Products",
+    timestamps: false,
+  },
+);
+
+export default Product;
