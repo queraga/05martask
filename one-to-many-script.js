9a279d9b5d9051938bd25e99193fbc83fb088641
Lashkov Yevhen <evgene@icloud.com>
2026-03-07 11:40:56 +0100

    Added one-to-many-script.js for creating Categories and Products

---
+++ one-to-many-script.js
@@ -0,0 +1,48 @@
+import db from "./models/index.js";
+const { sequelize, Category, Product } = db;
+
+async function run() {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection established successfully.");
+    await sequelize.sync({ force: true });
+    console.log("All tables recreated successfuly.");
+    const category = await Category.create({
+      name: "Electronics",
+    });
+    console.log("Category created:", category.toJSON());
+
+    const product1 = await Product.create({
+      name: "iPhone",
+      price: 999.99,
+      categoryId: category.id,
+    });
+
+    const product2 = await Product.create({
+      name: "MacBook",
+      price: 1999.99,
+      categoryId: category.id,
+    });
+
+    const product3 = await Product.create({
+      name: "iPad",
+      price: 799.99,
+      categoryId: category.id,
+    });
+
+    const product4 = await Product.create({
+      name: "Apple Watch",
+      price: 399.99,
+      categoryId: category.id,
+    });
+
+    console.log("Products created");
+    console.log(product1.toJSON());
+    console.log(product2.toJSON());
+    console.log(product3.toJSON());
+    console.log(product4.toJSON());
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+}
+run();
